898bccd37a4b0b838ab9d0c4f30b92d4f185883c
Anthony Sutton <anthony.sutton95@gmail.com>
2020-03-23 15:27:08 -0700

    Completed queue problem

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -1,19 +1,33 @@
-import sys
-sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
 
 class Queue:
     def __init__(self):
         self.size = 0
+
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+
+        # Set the storage to the linked list from DoublyLinkedList
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        pass
+        # Add 1 to the size of the storage and add whatever value is passed in
+        self.size += 1
+        self.storage.add_to_head(value)
 
     def dequeue(self):
-        pass
+        if self.size == 0:
+            return None
+
+        # We want to set the value to a variable so we can return it
+        else:
+            value = self.storage.tail.value
+            # Remove 1 from the size and call the helper function and remove the item
+            self.size -= 1
+            self.storage.remove_from_tail()
+
+            # return what was removed
+            return value
 
     def len(self):
-        pass
+        return self.storage.length
