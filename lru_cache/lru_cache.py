341e1f3ed8d0cf0582c246b3fcd35bdfd14a8f96
Anthony Sutton <anthony.sutton95@gmail.com>
2020-03-25 09:58:39 -0700

    Completed LRU with guided solution

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,6 @@
+from doubly_linked_list import DoublyLinkedList
+
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -6,8 +9,12 @@ class LRUCache:
     order, as well as a storage dict that provides fast access
     to every node stored in the cache.
     """
+
     def __init__(self, limit=10):
-        pass
+        self.limit = limit
+        self.size = 0
+        self.order = DoublyLinkedList()
+        self.storage = {}
 
     """
     Retrieves the value associated with the given key. Also
@@ -16,8 +23,16 @@ class LRUCache:
     Returns the value associated with the key or None if the
     key-value pair doesn't exist in the cache.
     """
+
     def get(self, key):
-        pass
+        # if key is not found, return None
+        if key not in self.storage:
+            return None
+        # if key is found, move to most recently used, return value
+        else:
+            node = self.storage[key]
+            self.order.move_to_end(node)
+            return node.value[1]
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -29,5 +44,30 @@ class LRUCache:
     want to overwrite the old value associated with the key with
     the newly-specified value.
     """
+
     def set(self, key, value):
-        pass
+
+        # What are the scenarios something might need to be set?
+
+        # if the item/key already exists
+        if key in self.storage:
+            # overwrite the value
+            node = self.storage[key]
+            node.value = (key, value)
+
+            # move to the tail (most recently used)
+            self.order.move_to_end(node)
+            return
+
+        # size is at limit
+        if len(self.order) == self.limit:
+            # evict the oldest one
+            index_of_oldest = self.order.head.value[0]
+            del self.storage[index_of_oldest]
+            self.order.remove_from_head()
+
+        # Add to the order
+        self.order.add_to_tail((key, value))
+
+        # Add to storage
+        self.storage[key] = self.order.tail
